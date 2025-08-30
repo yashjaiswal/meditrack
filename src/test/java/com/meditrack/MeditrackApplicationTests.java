2e70087183d290145f0d48d22bc0ed8d35967297
Yash Jaiswal <yashj009@gmail.com>
2025-08-30 21:07:01 +0530

    Initial commit

---
+++ src/test/java/com/meditrack/MeditrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
