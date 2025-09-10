69c839a4376616b0e119b875d97d8174d7774244
Yash Jaiswal <yashj009@gmail.com>
2025-09-10 15:29:01 +0530

    Added Spring security config

--- src/main/java/com/meditrack/app/SecurityConfig.java
+++ src/main/java/com/meditrack/app/SecurityConfig.java
@@ -1,13 +1,31 @@
 package com.meditrack.app;
 
+import com.meditrack.accounts.application.service.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // TODO
-    public SecurityFilterChain
+    public JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/accounts/signup", "/accounts/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+
+
+    }
 }
